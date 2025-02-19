7669b2e67592c6fed7d57a835e06dc032d1116e0
mishraditi <aditimishra91924@gmail.com>
2025-02-19 22:08:25 +0530

    Add test case for sum of an empty array returning zero

--- src/test/java/junit/ArrayTest.java
+++ src/test/java/junit/ArrayTest.java
@@ -1,2 +1,25 @@
-package junit;public class ArrayTest {
+package junit;
+
+import org.junit.jupiter.api.Test;
+import java.lang.annotation.Target;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayTest {
+
+//    @Test
+//    void test(){
+//        int[] numbers ={};
+//        array obj = new array();
+//       int resullt = obj.sumOfArray(numbers);
+//        System.out.println(resullt);
+//        int expectedResult = 0;
+//        assertEquals(expectedResult , resullt);
+//    }
+    @Test
+    void test1(){
+        array obj = new array();
+      int result = obj.sumOfArray(new int[] {});
+        assertEquals(0 , result);
+
+    }
 }
